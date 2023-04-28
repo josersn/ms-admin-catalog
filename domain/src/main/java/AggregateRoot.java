a7518cd7ca4eb834f080ff1f295e135d21767bef
josersn <nettorammos@hotmail.com>
2023-04-28 13:29:57 -0300

    base to add DDD

---
+++ domain/src/main/java/AggregateRoot.java
@@ -0,0 +1,5 @@
+public class AggregateRoot<ID extends Identifier> extends Entity<ID>{
+    public AggregateRoot(ID id) {
+        super(id);
+    }
+}
---
+++ domain/src/main/java/Identifier.java
@@ -0,0 +1,2 @@
+public abstract class Identifier extends ValueObject {
+}
